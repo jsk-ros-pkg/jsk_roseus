5ec511f214570f8865909a5d8dd0f907156fc61f
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2009-06-16 04:09:37 +0000

    fix comment outed data

--- roseus/setup-roseus.sh
+++ roseus/setup-roseus.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # patch for rosbuild
-#(cd ${HOME}/ros/ros/core/rosbuild;patch < ${OLDPWD}/rosbuild.cmake.for-roseus.diff)
+(cd ${HOME}/ros/ros/core/rosbuild;patch < ${OLDPWD}/rosbuild.cmake.for-roseus.diff)
 
 # generate link
-#ln -s ${HOME}/prog/roseus ${HOME}/ros/ros/core/experimental/roseus
+ln -s ${HOME}/prog/roseus ${HOME}/ros/ros/core/experimental/roseus
 
 ln -s  `rospack find roseus`/scripts/genmsg_eus `rospack find genmsg_cpp`/genmsg_eus  
 ln -s  `rospack find roseus`/scripts/gensrv_eus `rospack find genmsg_cpp`/gensrv_eus
