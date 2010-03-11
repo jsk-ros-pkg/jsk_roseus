7d6e16b8d0059736ef48d8d33f2ccb712d77ddda
ueda <ueda@jsk.t.u-tokyo.ac.jp>
2010-03-11 13:52:31 +0000

    fix for turtle

--- roseus/setup-roseus.sh
+++ roseus/setup-roseus.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-(cd ${HOME}/prog/roseus;mkdir cmake;cat ${HOME}/ros/pkgs/ros_experimental/rosoct/cmake/rosoct.cmake | sed -e 's/oct/eus/g' -e 's/".m"/".l"/g' > cmake/roseus.cmake)
+export ROSOCT_DIR=`rospack find rosoct`
+
+(cd ${HOME}/prog/roseus;mkdir cmake;cat ${ROSOCT_DIR}/cmake/rosoct.cmake | sed -e 's/oct/eus/g' -e 's/".m"/".l"/g' > cmake/roseus.cmake)
 
 # generate link
-ln -s ${HOME}/prog/roseus ${HOME}/ros/pkgs/ros_experimental/roseus
+ln -s ${HOME}/prog/roseus ${ROSOCT_DIR}/../roseus
 
 ln -s  `rospack find roseus`/scripts/genmsg_eus `rospack find genmsg_cpp`/genmsg_eus  
 ln -s  `rospack find roseus`/scripts/gensrv_eus `rospack find genmsg_cpp`/gensrv_eus
