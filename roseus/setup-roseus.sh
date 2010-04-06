0c643fce476001f5ad105d76de55d793fd6e3da3
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2010-04-06 13:52:04 +0000

    change auto generated .l filenaming rule from Package-Topic.l to Topic.l, and fix cmake.mk

--- roseus/setup-roseus.sh
+++ roseus/setup-roseus.sh
@@ -2,12 +2,10 @@
 
 export ROSOCT_DIR=`rospack find rosoct`
 
-(cd ${HOME}/prog/roseus;mkdir cmake;cat ${ROSOCT_DIR}/cmake/rosoct.cmake | sed -e 's/oct/eus/g' -e 's/".m"/".l"/g' > cmake/roseus.cmake)
-
 # generate link
 ln -s ${HOME}/prog/roseus ${ROSOCT_DIR}/../roseus
 
-ln -s  `rospack find roseus`/scripts/genmsg_eus `rospack find genmsg_cpp`/genmsg_eus  
+ln -s  `rospack find roseus`/scripts/genmsg_eus `rospack find genmsg_cpp`/genmsg_eus
 ln -s  `rospack find roseus`/scripts/gensrv_eus `rospack find genmsg_cpp`/gensrv_eus
 
 
