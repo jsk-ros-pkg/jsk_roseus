2d5a249695462e46449c1b1ba2e2fa35bd0c452f
youhei <youhei@jsk.imi.i.u-tokyo.ac.jp>
2010-04-23 15:03:59 +0000

    change for being able to locate roseus directory anywhere

--- roseus/setup-roseus.sh
+++ roseus/setup-roseus.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+ROSEUSDIR="$(dirname $0)"
+cd ${ROSEUSDIR}
+ROSEUSDIR=`pwd`
 export ROSOCT_DIR=`rospack find rosoct`
-
+echo "ROSEUS=$ROSEUSDIR"
 # generate link
-ln -s ${HOME}/prog/roseus ${ROSOCT_DIR}/../roseus
+ln -s ${ROSEUSDIR} ${ROSOCT_DIR}/../roseus
 
 ln -s  `rospack find roseus`/scripts/genmsg_eus `rospack find genmsg_cpp`/genmsg_eus
 ln -s  `rospack find roseus`/scripts/gensrv_eus `rospack find genmsg_cpp`/gensrv_eus
