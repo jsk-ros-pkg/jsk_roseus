ee46e48c4ad6e0f994f142de2917711f8a3d60f9
manabu <manabu@jsk.t.u-tokyo.ac.jp>
2010-08-18 17:08:02 +0000

    fix to use function generate-msg-srv

--- roseus/scripts/generate-all-msg-srv.sh
+++ roseus/scripts/generate-all-msg-srv.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 function generate-msg-srv {
-    local file=$1;
-    cd `rospack find $file`; rm -fr msg/eus srv/eus; if [ -e build ]; then cd build; make ROSBUILD_genmsg_eus; fi
+    local dir=$1;
+    echo $dir
+    cd $dir; rm -fr msg/eus srv/eus; if [ -e build ]; then cd build; make ROSBUILD_genmsg_eus; fi
 }
 
 
-for file in `rospack list-names`; do
-    echo $file
-    generate-msg-srv $file
+for pkg in `rospack list-names`; do
+    echo "package:$pkg"
+    generate-msg-srv `rospack find $pkg`
 done
 
-for dir in `rosstack list-names`; do
-    echo $dir
-    for file in `rosstack contents $dir`; do
-	echo $file
-	generate-msg-srv $file;
+for stack in `rosstack list-names`; do
+    echo "stack:$stack"
+    for dir in `rosstack contents $stack`; do
+	echo "  $dir"
+	generate-msg-srv `rosstack find $stack`/$dir
     done
 done
