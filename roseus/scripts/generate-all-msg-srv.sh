81a10aca63d9078d2075ea62b39d6eb198cd47f5
manabu <manabu@jsk.t.u-tokyo.ac.jp>
2010-08-18 16:53:09 +0000

    add debug print, echo  and echo  in stack

--- roseus/scripts/generate-all-msg-srv.sh
+++ roseus/scripts/generate-all-msg-srv.sh
@@ -1,8 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+
+function generate-msg-srv {
+    local file=$1;
+    cd `rospack find $file`; rm -fr msg/eus srv/eus; if [ -e build ]; then cd build; make ROSBUILD_genmsg_eus; fi
+}
 
 
 for file in `rospack list-names`; do
     echo $file
-    cd `rospack find $file`; rm -fr msg/eus srv/eus; if [ -e build ]; then cd build; make ROSBUILD_genmsg_eus; fi
+    generate-msg-srv $file
 done
 
+for dir in `rosstack list-names`; do
+    echo $dir
+    for file in `rosstack contents $dir`; do
+	echo $file
+	generate-msg-srv $file;
+    done
+done
