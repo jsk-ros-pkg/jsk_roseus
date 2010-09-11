0a2f6845d5253c7c004e476ffe9f52e2919ef5eb
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2010-09-11 09:25:34 +0000

    add IS-INITIALIZED

--- roseus/roseus.cpp
+++ roseus/roseus.cpp
@@ -1305,6 +1305,12 @@ pointer ROSEUS_GETNAME(register context *ctx,int n,pointer *argv)
 		    ros::this_node::getName().length()));
 }
 
+pointer ROSEUS_IS_INITIALIZED(register context *ctx,int n,pointer *argv)
+{
+  ckarg(0);
+  return(s_bInstalled?T:NIL);
+}
+
 /************************************************************
  *   __roseus
  ************************************************************/
@@ -1355,6 +1361,7 @@ pointer ___roseus(register context *ctx, int n, pointer *argv, pointer env)
 
   defun(ctx,"ROSPACK-FIND",argv[0],(pointer (*)())ROSEUS_ROSPACK_FIND);
   defun(ctx,"GET-NAME",argv[0],(pointer (*)())ROSEUS_GETNAME);
+  defun(ctx,"IS-INITIALIZED",argv[0],(pointer (*)())ROSEUS_IS_INITIALIZED);
 
   defun(ctx,"ROSEUS-RAW",argv[0],(pointer (*)())ROSEUS);
 
