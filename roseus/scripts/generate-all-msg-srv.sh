981497abde65c2f393a06cdc5d57a4a0b53923cc
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2011-02-03 04:56:00 +0000

    fix bug on genmanifest_eus

--- roseus/scripts/generate-all-msg-srv.sh
+++ roseus/scripts/generate-all-msg-srv.sh
@@ -61,7 +61,7 @@ for pkg_i in $(seq 0 $((${#pkg_list[@]} - 1))); do
 	mkdir -p $roshomedir/roseus/$pkg_name;
     fi
     depends=`rospack depends $pkg_name`
-    `rospack find roseus`/scripts/genmanifest_eus "$roshomedir/roseus/$pkg_name/_manifest.l" "$pkg_name" "$depends";
+    `rospack find roseus`/scripts/genmanifest_eus "$roshomedir/roseus/$pkg_name/_manifest.l" "$depends $pkg_name";
 done
 
 if [ $((${#err_list[@]})) -gt 0 ] ; then
