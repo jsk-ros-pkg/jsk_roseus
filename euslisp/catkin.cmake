93eeec525e68c6f31416d6d204aca278dde5c294
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2013-05-25 15:34:25 +0000

    install all files under SHARE/jskeus/eus.., force change Cflags of euslisp.pc and euslispConfig.cmake

--- euslisp/catkin.cmake
+++ euslisp/catkin.cmake
@@ -4,14 +4,6 @@ project(euslisp)
 
 find_package(catkin REQUIRED COMPONENTS rostest)
 
-catkin_package(
-    DEPENDS opengl libjpeg libx11-dev libxext libpng12
-    CATKIN-DEPENDS # TODO
-    INCLUDE_DIRS # TODO need to add ${EUSDIR}/include and other euslisp compile flags
-    LIBRARIES # TODO
-)
-
-
 # build euslisp
 execute_process(COMMAND cmake -E chdir ${PROJECT_SOURCE_DIR} make -f Makefile.eus
                 RESULT_VARIABLE _make_failed)
@@ -25,8 +17,16 @@ add_rostest(test/test-euslisp.launch)
 #  add_rostest(test/test-irtrobot.launch)
 #endif(disp)
 
+# build euslisp before catkin_package, since catkin_pakcage/INCLUDE_DIRS needs to be exists
+catkin_package(
+    DEPENDS opengl libjpeg libx11-dev libxext libpng12
+    CATKIN-DEPENDS # TODO
+    INCLUDE_DIRS jskeus/eus/include
+    LIBRARIES # TODO
+)
+
 # install under EUSDIR/ARCHDIR ...
-set(EUSDIR ${CATKIN_PACKAGE_SHARE_DESTINATION})
+set(EUSDIR ${CATKIN_PACKAGE_SHARE_DESTINATION}/jskeus/eus)
 if(${CMAKE_SYSTEM_NAME} MATCHES Linux)
   if(${CMAKE_SYSTEM_PROCESSOR} MATCHES amd64* OR
       ${CMAKE_SYSTEM_PROCESSOR} MATCHES x86_64* )
@@ -43,6 +43,11 @@ else()
 endif()
 message("-- Set EUSDIR  to ${CMAKE_INSTALL_PREFIX}/${EUSDIR}")
 message("-- Set ARCHDIR to ${ARCHDIR}")
+
+# could not found a way to set custom install directory for installed configuration file
+execute_process(COMMAND sed -i s@${CMAKE_INSTALL_PREFIX}/include@${CMAKE_INSTALL_PREFIX}/${EUSDIR}/include@ ${PROJECT_SOURCE_DIR}/build/catkin_generated/installspace/euslisp.pc ${PROJECT_SOURCE_DIR}/build/catkin_generated/installspace/euslispConfig.cmake)
+message("-- Force change Cflags of euslisp.pc and euslispConfig.cmake to ${CMAKE_INSTALL_PREFIX}/${EUSDIR}/include for installspace")
+
 
 # for all executable files under ${PROJECT_SOURCE_DIR}/jskeus/eus/Linux64/bin/
 file(GLOB executables "${PROJECT_SOURCE_DIR}/jskeus/eus/${ARCHDIR}/bin/*")
