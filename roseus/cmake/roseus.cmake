3d125e1bf5df9da0d481c7f7d12eebbc984b197b
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-04-02 19:31:47 +0900

    remove debug code

--- roseus/cmake/roseus.cmake
+++ roseus/cmake/roseus.cmake
@@ -1,5 +1,4 @@
 rosbuild_find_ros_package(geneus)
-message("--??aa>> ${ROSBUILD_roseus_${PROJECT_NAME}}---")
 if(NOT TARGET ROSBUILD_genmanifest_roseus_${PROJECT_NAME})
   include(${geneus_PACKAGE_PATH}/cmake/roseus.cmake)
 endif()
