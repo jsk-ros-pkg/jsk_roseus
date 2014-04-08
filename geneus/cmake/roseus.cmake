c54f5486007b3734c5427aa529492353ecceb07c
eisoku9618 <kuroiwa@jsk.imi.i.u-tokyo.ac.jp>
2014-04-08 21:32:49 +0900

    add depends to generate_messages_to_py

--- geneus/cmake/roseus.cmake
+++ geneus/cmake/roseus.cmake
@@ -47,11 +47,16 @@ if(NOT COMMAND rosbuild_find_ros_package) ## catkin
     file(GLOB ${arg_pkg}_MESSAGE_FILES "${pkg_full_path}/msg/*.msg")
     file(GLOB ${arg_pkg}_SERVICE_FILES "${pkg_full_path}/srv/*.srv")
 
+     if(${arg_pkg}_MESSAGE_FILES OR ${arg_pkg}_SERVICE_FILES) # check if we need to compile python message. generating eusmessage depends on python message to get to  know md5sum
+      set(_depend_generate_py ${arg_pkg}_generate_messages_py)
+    else()
+      set(_depend_generate_py )
+    endif()
     # gen manifest
     list(FIND ALL_GEN_OUTPUT_FILES_eus ${roseus_INSTALL_DIR}/${arg_pkg}/manifest.l _ret)
     if(${_ret} EQUAL -1)
       add_custom_command(OUTPUT ${roseus_INSTALL_DIR}/${arg_pkg}/manifest.l
-        DEPENDS genmanifest_eus #${${arg_pkg}_MESSAGE_FILES} ${${arg_pkg}_SERVICE_FILES}
+        DEPENDS genmanifest_eus ${_depend_generate_py}
         COMMAND ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH} ${GENMANIFEST_EUS} ${arg_pkg}
         COMMENT "Generating EusLisp code for upstream package ${arg_pkg}")
       list(APPEND ALL_GEN_OUTPUT_FILES_eus ${roseus_INSTALL_DIR}/${arg_pkg}/manifest.l)
@@ -64,7 +69,7 @@ if(NOT COMMAND rosbuild_find_ros_package) ## catkin
       list(FIND ALL_GEN_OUTPUT_FILES_eus ${roseus_INSTALL_DIR}/${arg_pkg}/msg/${_msg_name}.l _ret)
       if(${_ret} EQUAL -1)
         add_custom_command(OUTPUT ${roseus_INSTALL_DIR}/${arg_pkg}/msg/${_msg_name}.l
-          DEPENDS genmsg_eus ${_msg_file}
+          DEPENDS genmsg_eus ${_msg_file} ${_depend_generate_py}
           COMMAND ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH} PYTHONPATH=${CATKIN_DEVEL_PREFIX}/${CATKIN_GLOBAL_PYTHON_DESTINATION}:$ENV{PYTHONPATH} ${GENMSG_EUS} ${_msg_file}
           COMMENT "Generating EusLisp code for upstream message ${arg_pkg}/msg/${_msg_name}")
         list(APPEND ALL_GEN_OUTPUT_FILES_eus ${roseus_INSTALL_DIR}/${arg_pkg}/msg/${_msg_name}.l)
@@ -78,7 +83,7 @@ if(NOT COMMAND rosbuild_find_ros_package) ## catkin
       list(FIND ALL_GEN_OUTPUT_FILES_eus ${roseus_INSTALL_DIR}/${arg_pkg}/srv/${_srv_name}.l _ret)
       if(${_ret} EQUAL -1)
         add_custom_command(OUTPUT ${roseus_INSTALL_DIR}/${arg_pkg}/srv/${_srv_name}.l
-          DEPENDS gensrv_eus ${_srv_file}
+          DEPENDS gensrv_eus ${_srv_file} ${_depend_generate_py}
           COMMAND ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH} ${GENSRV_EUS} ${_srv_file}
           COMMENT "Generating EusLisp code for upstream service ${arg_pkg}/srv/${_srv_name}")
         list(APPEND ALL_GEN_OUTPUT_FILES_eus ${roseus_INSTALL_DIR}/${arg_pkg}/srv/${_srv_name}.l)
