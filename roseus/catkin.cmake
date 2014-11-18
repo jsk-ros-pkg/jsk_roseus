56211e71dd905517f39168483cf4d1423c20fc4b
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2014-11-18 13:57:39 +0900

    update param-test.l for testing parameter handling by roseus

--- roseus/catkin.cmake
+++ roseus/catkin.cmake
@@ -95,6 +95,7 @@ add_rostest(test/test-roseus.test)
 add_rostest(test/test-tf.test)
 add_rostest(test/test-disconnect.test)
 add_rostest(test/test-multi-queue.test)
+add_rostest(test/test-parameter.test)
 add_rostest(test/test-genmsg.catkin.test)
 add_rostest(test/test-genmsg-oneworkspace.catkin.launch) # use launch not to run on travis/catkin
 
