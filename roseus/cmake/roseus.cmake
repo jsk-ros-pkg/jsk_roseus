3ff34de60317f345e1cf79bafc4b66d46ac7e9e7
Kei Okada <kei.okada@gmail.com>
2015-02-13 02:12:00 +0900

    [roseus.cmake] add more condition

--- roseus/cmake/roseus.cmake
+++ roseus/cmake/roseus.cmake
@@ -46,6 +46,7 @@ macro(generate_all_roseus_messages)
   ## add target package those who does not have msg files
   if(NOT TARGET ${target_pkg}_generate_messages_eus AND
       NOT ${target_pkg}_MESSAGE_FILES AND NOT ${target_pkg}_SERVICE_FILES AND
+      NOT EXISTS ${PROJECT_SOURCE_DIR}/msg AND NOT EXISTS ${PROJECT_SOURCE_DIR}/srv AND
       NOT EXISTS ${roseus_PREFIX}/share/roseus/ros/${target_pkg})
     _list_append_unique(_${target_pkg}_generate_roseus_message_packages ${target_pkg})
   endif()
