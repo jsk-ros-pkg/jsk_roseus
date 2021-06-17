96a844a1830f5f68d8778d14aaf6699a438df274
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2021-06-17 12:56:52 +0900

    (roseus_bt) Add generate_package_xml

--- roseus_bt/include/roseus_bt/xml_parser.h
+++ roseus_bt/include/roseus_bt/xml_parser.h
@@ -44,6 +44,8 @@ public:
                                 const char* xml_filename);
   std::string generate_cmake_lists(const char* package_name,
                                    const char* target_filename);
+  std::string generate_package_xml(const char* package_name,
+                                   const char* author_name);
 
 };
 
@@ -566,6 +568,113 @@ target_link_libraries(%2% ${catkin_LIBRARIES})
     boost::algorithm::join(message_packages, "\n") %
     boost::algorithm::join(service_files, "\n") %
     boost::algorithm::join(action_files, "\n");
+
+  return bfmt.str();
+}
+
+std::string XMLParser::generate_package_xml(const char* package_name, const char* author_name) {
+  auto format_build_depend = [](std::string pkg) {
+    return fmt::format("  <build_depend>{}</build_depend>", pkg);
+  };
+  auto format_exec_depend = [](std::string pkg) {
+    return fmt::format("  <exec_depend>{}</exec_depend>", pkg);
+  };
+  auto maybe_push_message_package = [](const XMLElement* node, std::vector<std::string>* message_packages) {
+    std::string msg_type = node->Attribute("type");
+    std::size_t pos = msg_type.find('/');
+    if (pos != std::string::npos) {
+      std::string pkg = msg_type.substr(0, pos);
+      if (std::find(message_packages->begin(), message_packages->end(), pkg) ==
+          message_packages->end()) {
+        message_packages->push_back(pkg);
+      }
+    }
+  };
+
+  std::string author_email(author_name);
+  std::transform(author_email.begin(), author_email.end(), author_email.begin(),
+                 [](unsigned char c){ return std::tolower(c); });
+  std::replace(author_email.begin(), author_email.end(), ' ', '_');
+
+  const XMLElement* root = doc.RootElement()->FirstChildElement("TreeNodesModel");
+  std::vector<std::string> message_packages;
+
+  message_packages.push_back("std_msgs");
+  message_packages.push_back("actionlib_msgs");
+
+  for (auto node = root->FirstChildElement();
+       node != nullptr;
+       node = node->NextSiblingElement())
+    {
+      for (auto port_node = node->FirstChildElement();
+           port_node != nullptr;
+           port_node = port_node->NextSiblingElement())
+        {
+          maybe_push_message_package(port_node, &message_packages);
+        }
+    }
+
+  std::vector<std::string> build_dependencies;
+  std::vector<std::string> exec_dependencies;
+  build_dependencies.resize(message_packages.size());
+  exec_dependencies.resize(message_packages.size());
+
+  std::transform(message_packages.begin(), message_packages.end(),
+                 build_dependencies.begin(), format_build_depend);
+  std::transform(message_packages.begin(), message_packages.end(),
+                 exec_dependencies.begin(), format_exec_depend);
+
+  std::string fmt_string = 1 + R"(
+<?xml version="1.0"?>
+<package format="2">
+  <name>%1%</name>
+  <version>0.0.0</version>
+  <description>The %1% package</description>
+
+  <!-- One maintainer tag required, multiple allowed, one person per tag -->
+  <maintainer email="%3%@example.com">%2%</maintainer>
+
+
+  <!-- One license tag required, multiple allowed, one license per tag -->
+  <!-- Commonly used license strings: -->
+  <!--   BSD, MIT, Boost Software License, GPLv2, GPLv3, LGPLv2.1, LGPLv3 -->
+  <license>TODO</license>
+
+
+  <!-- Url tags are optional, but multiple are allowed, one per tag -->
+  <!-- Optional attribute type can be: website, bugtracker, or repository -->
+  <!-- Example: -->
+  <!-- <url type="website">http://wiki.ros.org/%1%</url> -->
+
+
+  <!-- Author tags are optional, multiple are allowed, one per tag -->
+  <!-- Authors do not have to be maintainers, but could be -->
+  <!-- Example: -->
+  <!-- <author email="%3%@example.com">%2%</author> -->
+
+
+  <buildtool_depend>catkin</buildtool_depend>
+  <build_depend>message_generation</build_depend>
+  <build_depend>roscpp</build_depend>
+  <build_depend>roseus_bt</build_depend>
+%4%
+
+  <exec_depend>message_runtime</exec_depend>
+  <exec_depend>roscpp</exec_depend>
+  <exec_depend>roseus_bt</exec_depend>
+%5%
+
+  <export>
+  </export>
+</package>
+)";
+
+  boost::format bfmt = boost::format(fmt_string) %
+    package_name %
+    author_name %
+    author_email %
+    boost::algorithm::join(build_dependencies, ",\n") %
+    boost::algorithm::join(exec_dependencies, ",\n");
 
   return bfmt.str();
 }
