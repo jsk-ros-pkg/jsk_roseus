8db457d62c3d013414256315fcc6364d1e926e73
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2021-06-17 16:44:03 +0900

    (roseus_bt) Change action/service file generator

--- roseus_bt/src/test_parse.cpp
+++ roseus_bt/src/test_parse.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <roseus_bt/xml_parser.h>
 
-
 int main(int argc, char **argv)
 {
   RoseusBT::XMLParser parser(argv[1]);
 
   // std::cout << parser.generate_cpp_file("my_package", "test", argv[1]) << std::endl;
   // std::cout << parser.generate_cmake_lists("my_package", "test") << std::endl;
-  std::cout << parser.generate_package_xml("my_package", "Guilherme Affonso") << std::endl;
+  // std::cout << parser.generate_package_xml("my_package", "Guilherme Affonso") << std::endl;
 
-  // std::cout << "Actions:" << std::endl;
-  // std::cout << parser.test_all_actions() << std::endl;
+  std::map<std::string, std::string> action_files, service_files;
+  std::map<std::string, std::string>::iterator it;
 
-  // std::cout << "Conditions:" << std::endl;
-  // std::cout << parser.test_all_conditions() << std::endl;
+  action_files = parser.generate_all_action_files();
+  service_files = parser.generate_all_service_files();
+
+  for (it=action_files.begin(); it!=action_files.end(); ++it) {
+    std::cout << it->first << std::endl;
+    std::cout << it->second << std::endl << std::endl;
+  }
+
+  for (it=service_files.begin(); it!=service_files.end(); ++it) {
+    std::cout << it->first << std::endl;
+    std::cout << it->second << std::endl << std::endl;
+  }
 
   return 0;
 }
