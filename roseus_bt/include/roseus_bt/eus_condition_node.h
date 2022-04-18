46ccea6b66787373f447fad0b75d9679e4cee059
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2022-04-18 13:29:09 +0900

    (roseus_bt) Check server connections at init time

--- roseus_bt/include/roseus_bt/eus_condition_node.h
+++ roseus_bt/include/roseus_bt/eus_condition_node.h
@@ -10,7 +10,17 @@ template<class ServiceT>
 class EusConditionNode : public BT::RosServiceNode<ServiceT>
 {
 protected:
- EusConditionNode(ros::NodeHandle& nh, const std::string& name, const BT::NodeConfiguration& conf): BT::RosServiceNode<ServiceT>(nh, name, conf) {};
+ EusConditionNode(ros::NodeHandle& nh, const std::string& name, const BT::NodeConfiguration& conf): BT::RosServiceNode<ServiceT>(nh, name, conf) {
+   const unsigned msec = BT::TreeNode::getInput<unsigned>("timeout").value();
+   const std::string server_name = BT::TreeNode::getInput<std::string>("service_name").value();
+   ros::Duration timeout(static_cast<double>(msec) * 1e-3);
+
+   ROS_DEBUG("Connecting to service server at '%s'...", server_name.c_str());
+   bool connected = BT::RosServiceNode<ServiceT>::service_client_.waitForExistence(timeout);
+   if(!connected){
+     throw BT::RuntimeError("Couldn't connect to service server at: ", server_name);
+   }
+ };
 
 public:
   EusConditionNode() = delete;
