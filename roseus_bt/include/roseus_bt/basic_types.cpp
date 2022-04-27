778ee2b9af4347c5393398dea2b3c6292fdc4e30
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2022-04-27 20:39:44 +0900

    (roseus_bt) Add basic_types.h

---
+++ roseus_bt/include/roseus_bt/basic_types.cpp
@@ -0,0 +1,51 @@
+#include "roseus_bt/basic_types.h"
+
+namespace roseus_bt
+{
+
+std::string toStr(NodeType type)
+{
+    switch (type)
+    {
+        case NodeType::ACTION:
+            return "Action";
+        case NodeType::CONDITION:
+            return "Condition";
+        case NodeType::DECORATOR:
+            return "Decorator";
+        case NodeType::CONTROL:
+            return "Control";
+        case NodeType::SUBTREE:
+            return "SubTree";
+        case NodeType::SUBSCRIBER:
+            return "Subscriber";
+        case NodeType::REMOTE_ACTION:
+            return "RemoteAction";
+        case NodeType::REMOTE_CONDITION:
+            return "RemoteCondition";
+        default:
+            return "Undefined";
+    }
+}
+
+NodeType convertFromString(BT::StringView str)
+{
+    if( str == "Action" )    return NodeType::ACTION;
+    if( str == "Condition" ) return NodeType::CONDITION;
+    if( str == "Control" )   return NodeType::CONTROL;
+    if( str == "Decorator" ) return NodeType::DECORATOR;
+    if( str == "SubTree" || str == "SubTreePlus" ) return NodeType::SUBTREE;
+    if( str == "Subscriber" )      return NodeType::SUBSCRIBER;
+    if( str == "RemoteAction" )    return NodeType::ACTION;
+    if( str == "RemoteCondition" ) return NodeType::CONDITION;
+    return NodeType::UNDEFINED;
+}
+
+std::ostream& operator<<(std::ostream& os, const NodeType& type)
+{
+    os << toStr(type);
+    return os;
+}
+
+
+}  // namespace roseus_bt
