3b4af6541717b9a96390d0ac15924281c9a4e38a
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2022-10-21 10:45:56 +0900

    (roseus_bt) Add CopyDocument class to avoid unnecessary dumping and parsing

---
+++ roseus_bt/include/roseus_bt/copy_document.h
@@ -0,0 +1,66 @@
+#ifndef JSON_COPY_DOCUMENT_
+#define JSON_COPY_DOCUMENT_
+
+
+#include "rapidjson/include/rapidjson/document.h"
+#include "rapidjson/include/rapidjson/writer.h"
+#include "rapidjson/include/rapidjson/stringbuffer.h"
+
+
+namespace rapidjson
+{
+class CopyDocument : public rapidjson::Document
+{
+public:
+  CopyDocument() : Document() {}
+
+  CopyDocument(const CopyDocument& document) {
+    _clone(document);
+  }
+
+  CopyDocument(const Document& document) {
+    _clone(document);
+  }
+
+  CopyDocument(CopyDocument&& document) : Document(std::move(document)) {}
+
+  CopyDocument(Document&& document) : Document(std::move(document)) {}
+
+  CopyDocument& operator=(const CopyDocument& document) {
+    _clone(document);
+    return *this;
+  }
+
+  CopyDocument& operator=(const Document& document) {
+    _clone(document);
+    return *this;
+  }
+
+  CopyDocument& operator=(CopyDocument&& document) {
+    Swap(document);
+    return *this;
+  }
+
+  CopyDocument& operator=(Document&& document) {
+    Swap(document);
+    return *this;
+  }
+
+  std::string toStr() const
+  {
+    StringBuffer strbuf;
+    Writer<StringBuffer> writer(strbuf);
+    Accept(writer);
+    return strbuf.GetString();
+  }
+
+protected:
+  void _clone(const Document& document) {
+    SetObject();
+    CopyFrom(document, GetAllocator());
+  }
+};
+
+}
+
+#endif  // JSON_COPY_DOCUMENT
