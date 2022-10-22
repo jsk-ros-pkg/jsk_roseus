b3645fa781f1c31558f41575087286519277e00f
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2022-10-22 16:21:33 +0900

    (roseus_bt) Decrease sleep time in remote nodes

--- roseus_bt/include/roseus_bt/ws_action_client.h
+++ roseus_bt/include/roseus_bt/ws_action_client.h
@@ -76,7 +76,7 @@ public:
   void waitForResult() {
     std::cout << "RemoteAction: waiting for result: " << result_topic_ << std::endl;
     while (is_active_) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
   }
 
--- roseus_bt/include/roseus_bt/ws_service_client.h
+++ roseus_bt/include/roseus_bt/ws_service_client.h
@@ -44,7 +44,7 @@ public:
   void waitForResult() {
     std::cout << "RemoteService: waiting for result: " << service_name_ << std::endl;
     while (is_active_) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
   }
 
