b4b52b8c032d96a69a16a77065db62d1ca2eb7af
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-11-21 18:31:33 +0900

    add common_argument_mapping.h

--- roseus_bt/include/roseus_bt/command_line_argument_mapping.h
+++ roseus_bt/include/roseus_bt/command_line_argument_mapping.h
@@ -10,6 +10,7 @@
 #include <behaviortree_cpp_v3/bt_factory.h>
 #include <ros/init.h>
 
+#include <roseus_bt/common_argument_mapping.h>
 
 namespace roseus_bt
 {
@@ -65,21 +66,6 @@ bool parse_command_line(int argc, char** argv,
     }
   }
   return true;
-}
-
-void register_blackboard_variables(BT::Tree* tree,
-                                   const std::map<std::string, std::string>& argument_map)
-{
-  // New variables are registered as strings.
-  // However, if the port has already been registered
-  // in the tree, it can be default-casted.
-  for (const auto it: argument_map) {
-#ifdef DEBUG
-    std::cout << "Initializing blackboard variable: " << it.first <<
-      " with value: " << it.second << "\n";
-#endif
-    tree->rootBlackboard()->set<std::string>(it.first, it.second);
-  }
 }
 
 }  // namespace roseus_bt
